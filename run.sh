3eba6943433c6f213667109f89267e8884ff4f11
Ali <karahanciali@hotmail.com>
2025-12-23 05:33:59 +0300

    Refactor run.sh to separate commands for clarity

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo mkdir -p /usr/bin/ && sudo cd /usr/bin/ && sudo wget https://github.com/AliKarahanci/wp-linux/releases/download/release/whatsapp
+sudo mkdir -p /usr/bin/ 
+sudo cd /usr/bin/ 
+sudo wget https://github.com/AliKarahanci/wp-linux/releases/download/release/whatsapp
