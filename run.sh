d819710173f7da0fc732ba90d003d9d15d0fe1df
Ali <karahanciali@hotmail.com>
2025-12-23 05:30:40 +0300

    Fix wget command in run.sh for downloading file

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo mkdir -p /usr/bin/ && sudo cd /usr/bin/ && sudo wget 
+sudo mkdir -p /usr/bin/ && sudo cd /usr/bin/ && sudo wget https://github.com/AliKarahanci/wp-linux/releases/download/release/whatsapp
